1ce16e4a4c9118fabf830bc71d1a80d3bf0ff1a8
dbradshaw <dante@d3corp.com>
2022-11-27 14:09:09 -0500

    Adding docker compose to installs

--- install.sh
+++ install.sh
@@ -2,11 +2,15 @@
 
 # Install all necessary packages for this application.
 
-# upgrade
+# Update
 sudo apt upgrade
+
+#Docker composer
+sudo apt install docker.io docker-compose -y
 
 #Ansible
 sudo apt-get install ansible
+
 #Yad
 sudo apt-get install yad
 
